6d4a26accbea6a0aada6997443492c1d3df6511c
harkl <3zy3@protonmail.ch>
2023-09-19 14:13:01 +1200

    resolve migration issues

--- prisma/migrations/20230919021204_tiplinks/migration.sql
+++ prisma/migrations/20230919021204_tiplinks/migration.sql
@@ -3,9 +3,10 @@ CREATE TABLE "TipLink" (
     "id" SERIAL NOT NULL,
     "tipLink" TEXT NOT NULL,
     "signature" TEXT NOT NULL,
+    "mintAddress" TEXT NOT NULL,
+    "sender" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "mint" TEXT NOT NULL,
 
     CONSTRAINT "TipLink_pkey" PRIMARY KEY ("id")
 );
@@ -17,4 +18,4 @@ CREATE UNIQUE INDEX "TipLink_tipLink_key" ON "TipLink"("tipLink");
 CREATE UNIQUE INDEX "TipLink_signature_key" ON "TipLink"("signature");
 
 -- AddForeignKey
-ALTER TABLE "TipLink" ADD CONSTRAINT "TipLink_mint_fkey" FOREIGN KEY ("mintAddress") REFERENCES "Mint"("mintAddress") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "TipLink" ADD CONSTRAINT "TipLink_mintAddress_fkey" FOREIGN KEY ("mintAddress") REFERENCES "Mint"("mintAddress") ON DELETE RESTRICT ON UPDATE CASCADE;
--- scripts/reset-dev.sh
+++ scripts/reset-dev.sh
@@ -4,5 +4,8 @@ docker exec -it mixtape_postgres_1 psql -U postgres -c "DROP DATABASE IF EXISTS 
 # Create a new database
 docker exec -it mixtape_postgres_1 psql -U postgres -c "CREATE DATABASE mixtape_dev;"
 
+# Copy the file to backup.sql
+docker cp ./backup-merge.sql mixtape_postgres_1:/backup.sql
+
 # Restore the database from the backup
 docker exec -it mixtape_postgres_1 psql -U postgres -d mixtape_dev -f /backup.sql
