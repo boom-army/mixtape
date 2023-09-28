514aebeb75eb885a037a6724193a5041b4a24fa1
harkl <3zy3@protonmail.ch>
2023-09-28 17:13:21 +1300

    add new templates and supply

--- prisma/migrations/20230927042640_nfttemplate/migration.sql
+++ prisma/migrations/20230927042640_nfttemplate/migration.sql
@@ -9,7 +9,7 @@ CREATE TABLE "NftTemplate" (
     "image" TEXT NOT NULL,
     "releaseDate" TIMESTAMP(3) NOT NULL,
     "endDate" TIMESTAMP(3),
-    "maxQuantity" INTEGER,
+    "maxSupply" INTEGER,
     "status" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -18,12 +18,13 @@ CREATE TABLE "NftTemplate" (
 );
 
 -- Insert default rows
-INSERT INTO "NftTemplate" ("id", "name", "runtime", "price", "image", "releaseDate", "endDate", "maxQuantity", "status", "createdAt", "updatedAt")
+INSERT INTO "NftTemplate" ("id", "name", "runtime", "price", "image", "releaseDate", "endDate", "maxSupply", "status", "createdAt", "updatedAt")
 VALUES 
 ('123e4567-e89b-12d3-a456-426614174000', 'MixtApe', 30, 0.1, '/images/mixtape-1024.png', '2023-09-08 00:00:00', NULL, NULL, 'active', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 ('123e4567-e89b-12d3-a456-426614174001', 'Brite', 30, 0.1, '/images/mixtape-rainbow-1024.png', '2023-09-06 00:00:00', NULL, NULL, 'active', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('123e4567-e89b-12d3-a456-426614174002', 'Shoey', 30, 0.1337, '/images/mixtape-shoey.png', '2023-09-19 00:00:00', NULL, NULL, 'active', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('123e4567-e89b-12d3-a456-426614174003', 'SOL', 30, 0.142069, '/images/mixtape-sol-1024.png', '2023-09-06 00:00:00', NULL, NULL, 'active', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+('123e4567-e89b-12d3-a456-426614174002', 'Shoey', 30, 0.1, '/images/mixtape-shoey.png', '2023-09-19 00:00:00', '2023-10-05 00:00:00', NULL, 'active', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('123e4567-e89b-12d3-a456-426614174003', 'SOL', 30, 0.1, '/images/mixtape-sol-1024.png', '2023-09-06 00:00:00', NULL, NULL, 'active', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('123e4567-e89b-12d3-a456-426614174004', 'APE', 30, 0.1, '/images/mixtape-tdk-1024.png', '2023-09-28 00:00:00', NULL, 10, 'active', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 -- AlterTable
 ALTER TABLE "Mint" ADD COLUMN "templateId" TEXT;
