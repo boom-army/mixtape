fe2c03ef0da52054bf929db088e52b46789013b7
harkl <3zy3@protonmail.ch>
2023-10-02 10:28:36 +1300

    add tracks api

---
+++ src/pages/api/add-template.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../lib/prisma";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const { name, runtime, price, image, releaseDate, endDate, maxSupply, status, password } = req.body;
+
+    if (password !== "storeroom-utopia-path-cardboard") {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    try {
+      const newTemplate = await prisma.nftTemplate.create({
+        data: {
+          name,
+          runtime,
+          price,
+          image,
+          releaseDate,
+          endDate,
+          maxSupply,
+          status
+        },
+      });
+
+      return res.status(200).json(newTemplate);
+    } catch (error) {
+      return res.status(500).json({ error: "Failed to create NFT template", details: error instanceof Error ? error.message : "Unknown error" });
+    }
+  } else {
+    return res.status(405).json({ error: "Method not allowed. Please use POST" });
+  }
+};
+
+export default handler;
