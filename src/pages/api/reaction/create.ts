1ebda621d6737a237772162b1d54262120274eec
harkl <3zy3@protonmail.ch>
2023-10-06 17:16:50 +1300

    wrong file

---
+++ src/pages/api/reaction/create.ts
@@ -0,0 +1,38 @@
+import prisma from "../../../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function createReaction(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { userId, emoteId, mintAddress } = req.body;
+
+  if (!userId || !emoteId || !mintAddress) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  try {
+    await prisma.$transaction([
+      prisma.mintEmote.create({
+        data: {
+          mintAddress,
+          emoteId,
+          userId,
+        },
+      }),
+      prisma.points.create({
+        data: {
+          points: 1,
+          userId,
+          mintAddress,
+          awardedId: "reaction",
+        },
+      }),
+    ]);
+
+    res.json({ ok: true });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: (error as Error).message });
+  }
+}
