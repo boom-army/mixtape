627fe8015ea0ecb2452b3ca3a37cf3055f2f3aa8
harkl <3zy3@protonmail.ch>
2023-10-06 13:52:55 +1300

    set back from script

--- scripts/reset-dev.sh
+++ scripts/reset-dev.sh
@@ -5,7 +5,7 @@ docker exec -it mixtape_postgres_1 psql -U postgres -c "DROP DATABASE IF EXISTS 
 docker exec -it mixtape_postgres_1 psql -U postgres -c "CREATE DATABASE mixtape_dev;"
 
 # Copy the file to backup.sql
-docker cp ./backup-dev.sql mixtape_postgres_1:/backup.sql
+docker cp ./scripts/backup-dev-db-23-10-06.sql mixtape_postgres_1:/backup.sql
 
 # Restore the database from the backup
 docker exec -it mixtape_postgres_1 psql -U postgres -d mixtape_dev -f /backup.sql
