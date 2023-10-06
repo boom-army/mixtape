cb34b4762ff8914604cdb17c54868ea836e850cd
harkl <3zy3@protonmail.ch>
2023-10-07 11:56:34 +1300

    find or create user

--- src/pages/api/reaction/create.ts
+++ src/pages/api/reaction/create.ts
@@ -6,12 +6,23 @@ export default async function createReaction(
   res: NextApiResponse
 ) {
   const { userId, emoteId, mintAddress } = req.body;
+  console.log({ userId, emoteId, mintAddress });
+  
 
   if (!userId || !emoteId || !mintAddress) {
     return res.status(400).json({ error: "Missing required fields" });
   }
 
   try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+    if (!user) {
+      await prisma.user.create({
+        data: { id: userId },
+      });
+    }
+
     await prisma.$transaction([
       prisma.mintEmote.create({
         data: {
