8a369fc931b77dae16ae41c6dbf11393167c8bc4
harkl <3zy3@protonmail.ch>
2023-10-14 08:33:34 +1300

    scaffold mixtape points

---
+++ src/pages/api/points/mix-leaders.ts
@@ -0,0 +1,45 @@
+// src/pages/api/mints/top.ts
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../../lib/prisma";
+
+export default async function topMints(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    try {
+      const mints = await prisma.mint.findMany({
+        select: {
+          mintAddress: true,
+        },
+      });
+
+      const topMints = (await Promise.all(
+        mints.map(async (mint) => {
+          const totalPoints = await prisma.points.aggregate({
+            _sum: {
+              points: true,
+            },
+            where: {
+              mintAddress: mint.mintAddress,
+            },
+          });
+          return {
+            mintAddress: mint.mintAddress,
+            totalPoints: totalPoints._sum.points || 0,
+          };
+        })
+      )).filter(mint => mint.totalPoints > 0);
+
+      const sortedMints = topMints.sort((a, b) => b.totalPoints - a.totalPoints);
+
+      res.status(200).json({ topMints: sortedMints });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "An error occurred while fetching the top mints." });
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
--- src/pages/leaderboards.tsx
+++ src/pages/leaderboards.tsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
-export default function Leaderboards() {
+export default function TopMints() {
+  const [topMints, setTopMints] = useState([]);
+
+  useEffect(() => {
+    const fetchTopMints = async () => {
+      try {
+        const response = await fetch('/api/points/mix-leaders');
+        const data = await response.json();
+        console.log('data', data);
+        
+        setTopMints(data.topMints);
+      } catch (error) {
+        console.error('Failed to fetch top mints:', error);
+      }
+    };
+    fetchTopMints();
+  }, []);
+
   return (
-    <div>leaderboards</div>
-  )
+    <TableContainer>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Mint Address</TableCell>
+            <TableCell align="right">Points</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {topMints.map((row: any) => (
+            <TableRow key={row.mintAddress}>
+              <TableCell component="th" scope="row">
+                {row.mintAddress}
+              </TableCell>
+              <TableCell align="right">{row.totalPoints}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
