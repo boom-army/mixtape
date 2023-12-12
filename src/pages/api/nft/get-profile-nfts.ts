a66a4b1811aef150adc1c60306d28d237e9215cf
harkl <3zy3@protonmail.ch>
2023-12-13 12:47:03 +1300

    scaffold sortby

--- src/pages/api/nft/get-profile-nfts.ts
+++ src/pages/api/nft/get-profile-nfts.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Helius } from "helius-sdk";
 import { getCluster } from "../../../utils";
+import { AssetSortBy, AssetSortDirection } from "helius-sdk";
 
 const helius = new Helius(process.env.NEXT_HELIUS_RPC_KEY!, getCluster());
 
@@ -12,6 +13,10 @@ async function getNFTsByOwner(ownerAddress: string, page = 1) {
       showCollectionMetadata: true,
       showUnverifiedCollections: false,
     },
+    // sortBy: {
+    //   sortBy: AssetSortBy.Created,
+    //   sortDirection: AssetSortDirection.Desc,
+    // },
   });
   return response;
 }
@@ -32,19 +37,21 @@ export default async function handler(
 
   try {
     const data = await getNFTsByOwner(address as string, Number(page) ?? 1);
-    const filteredData = data.items
-      .filter((nft) =>
-        nft?.authorities?.some(
-          (authority) =>
-            authority.address === "MXTPExF3AYg6bW31ucCWHjh2wYaoDsF1Kx8jbHpD41N"
-        )
-      )
-      .map((nft) => ({
-        mint: nft.id,
-        image: nft?.content?.links?.image,
-        meta: nft?.content?.metadata,
-      }));
-    res.status(200).json({ profileNfts: filteredData });
+    console.log(data);
+    
+    // const filteredData = data.items
+    //   .filter((nft) =>
+    //     nft?.authorities?.some(
+    //       (authority) =>
+    //         authority.address === "MXTPExF3AYg6bW31ucCWHjh2wYaoDsF1Kx8jbHpD41N"
+    //     )
+    //   )
+    //   .map((nft) => ({
+    //     mint: nft.id,
+    //     image: nft?.content?.links?.image,
+    //     meta: nft?.content?.metadata,
+    //   }));
+    // res.status(200).json({ profileNfts: filteredData });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Failed to fetch NFTs by owner" });
